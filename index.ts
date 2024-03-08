8626a8db01933a1fb1090d9377fe42b4c8ad8a12
Uzma imran <148060110+uzmaimran970@users.noreply.github.com>
2024-03-08 16:21:28 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,17 @@
+10.// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+// program written by uzma
+
+// today's date is 25-2-2024
+
+// this is our first program hello world
+
+console.log("hello world");
+
+// program written by uzma
+
+// today's date is 25-2-2024
+
+// this is our first program addition two digits
+
+console.log (2+2);
